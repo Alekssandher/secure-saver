3d1e3cd26e1cd601af15d5a3c5c0964f2ff4cfc6
Alekssandher <alekssandher1@hotmail.com>
2024-12-16 12:00:54 -0300

    fix: foot url

--- scripts/Footer/Footer.cs
+++ scripts/Footer/Footer.cs
@@ -3,7 +3,7 @@ using System;
 
 public partial class Footer : HBoxContainer
 {
-	string url = "https://bing.com";
+	string url = "https://github.com/Alekssandher/secure-saver";
 	private void _on_button_pressed() {
 		OS.ShellOpen(url);
 	}
